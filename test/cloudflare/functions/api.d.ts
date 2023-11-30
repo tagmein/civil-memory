37b729a91cd0702831f82c26eb7dd9e06e22691c
Nate Ferrero <nate@tagme.in>
2023-11-29 23:01:47 -0500

    update git ignore

--- test/cloudflare/functions/api.d.ts
+++
@@ -1,8 +0,0 @@
-import type { KVNamespace, PagesFunction } from '@cloudflare/workers-types';
-interface Env {
-    DATA_KV: KVNamespace;
-}
-export declare const onRequestGet: PagesFunction<Env>;
-export declare const onRequestDelete: PagesFunction<Env>;
-export declare const onRequestPost: PagesFunction<Env>;
-export {};
--- test/vercel/api/index.d.ts
+++
@@ -1,2 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-export default function (request: VercelRequest, response: VercelResponse): Promise<VercelResponse>;
